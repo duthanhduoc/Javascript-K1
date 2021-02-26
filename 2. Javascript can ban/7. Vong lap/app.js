01f80d9b5c67477c64a32e9696fcabe7575c6270
Du Thanh Duoc <duoc.dt@neo-lab.vn>
2021-02-26 08:59:02 +0700

    Buoi 1

---
+++ 2. Javascript can ban/7. Vong lap/app.js
@@ -0,0 +1,35 @@
+// let a = 1
+// while (a <= 2) {
+//   console.log(a)
+//   a++
+// }
+
+// let a = 1
+// do {
+//   console.log(a)
+//   a++
+// } while (a <= 2)
+
+// for (let i = 1; i <= 10; i = i + 2) {
+//   console.log(i)
+// }
+
+// let person = 'hello'
+
+// for (let x of person) {
+//   console.log(x)
+// }
+
+// while (true) {
+//   console.log('hello')
+// }
+
+// if (a === false) {
+//   console.log('dung')
+// } else {
+//   console.log('sai')
+// }
+// console.log(a)
+
+let a = false ? console.log('dung') : console.log('sai')
+console.log(a)
---
+++ test/app.js
@@ -0,0 +1,2 @@
+console.log(111)
+let a = 10
