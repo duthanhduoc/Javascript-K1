51aeae38fbf84d9bf54a1115b9484991790bf35b
Duoc <duthanhduoc@gmail.com>
2021-03-11 22:03:35 +0700

    update

--- 4. DOM project/3. Project game keo bua bao/app.js
+++ 4. DOM project/3. Project game keo bua bao/app.js
@@ -1,65 +1,78 @@
+const VALUES = [
+  { id: 'scissors', value: '✌' },
+  { id: 'rock', value: '✊' },
+  { id: 'paper', value: '✋' }
+]
 let i = 0
+
+// Change
 const change = () => {
-  const spans = document.querySelectorAll('.b span')
-  spans.forEach(
-    (item) =>
-      item.classList.contains('d-block') && item.classList.remove('d-block')
-  )
-  spans[i].classList.add('d-block')
-  if (i === spans.length - 1) {
+  const computer = document.querySelector('.computer')
+  computer.textContent = VALUES[i].value
+  computer.dataset.id = VALUES[i].id
+  if (i === VALUES.length - 1) {
     i = 0
   } else {
     i++
   }
 }
-let intervalChange = setInterval(change, 100)
-const firstWinSecond = (value1, value2) => {
-  const arr = ['scissors', 'rock', 'paper']
-  if (arr.indexOf(value1) === 0 && arr.indexOf(value2) === arr.length - 1) {
+
+let interval = setInterval(change, 100)
+
+// Compare
+const compare = (value1, value2) => {
+  const index1 = VALUES.findIndex((item) => item.id === value1)
+  const index2 = VALUES.findIndex((item) => item.id === value2)
+  if (index1 === 0 && index2 === VALUES.length - 1) {
     return 1
-  } else if (value1 === value2) {
+  } else if (index1 === VALUES.length - 1 && index2 === 0) {
+    return -1
+  } else if (index1 === index2) {
     return 0
+  } else {
+    return index1 < index2 ? -1 : 1
   }
-  return arr.indexOf(value1) > arr.indexOf(value2) ? 1 : -1
 }
 
-document.querySelectorAll('.a .btn').forEach((item) => {
+// User click
+document.querySelectorAll('.user').forEach((item) => {
   item.addEventListener('click', (event) => {
-    if (!item.classList.contains('active')) {
-      clearInterval(intervalChange)
-      item.classList.add('active')
-      document
-        .querySelectorAll('.a .btn:not(.active)')
-        .forEach((item) => item.setAttribute('disabled', ''))
-      const value1 = item.dataset.value
-      const value2 = document.querySelector('.b span.d-block').dataset.value
-      const result = firstWinSecond(value1, value2)
-      const notificationNode = document.querySelector('.notification')
-      const alertNode = document.createElement('div')
-      notificationNode.appendChild(alertNode)
-      if (result === -1) {
-        alertNode.classList.add('d-inline-block', 'alert', 'alert-dark')
-        alertNode.textContent = 'Bạn đã thua'
-      } else if (result === 0) {
-        alertNode.classList.add('d-inline-block', 'alert', 'alert-primary')
-        alertNode.textContent = 'Bạn đã hòa'
-      } else {
-        alertNode.classList.add('d-inline-block', 'alert', 'alert-success')
-        alertNode.textContent = 'Bạn đã thắng'
-      }
-      document.querySelector('.btn__play-again').classList.remove('d-none')
+    event.target.classList.add('active')
+    const value1 = event.target.id
+    clearInterval(interval)
+    const value2 = document.querySelector('.computer').dataset.id
+    const result = compare(value1, value2)
+    let message
+    document.querySelector('.notification').classList.remove('d-none')
+    const alert = document.querySelector('.notification .alert')
+    if (result === -1) {
+      message = 'Bạn đã thua'
+      alert.classList.add('alert-dark')
+    } else if (result === 0) {
+      message = 'Bạn đã hòa'
+      alert.classList.add('alert-warning')
+    } else {
+      message = 'Bạn đã thắng'
+      alert.classList.add('alert-success')
     }
+    alert.textContent = message
+    document.querySelector('.play-again').classList.remove('d-none')
+    document.querySelectorAll('.user').forEach((_item) => {
+      _item.style.pointerEvents = 'none'
+    })
   })
 })
 
-document
-  .querySelector('.btn__play-again')
-  .addEventListener('click', (event) => {
-    document.querySelector('.btn__play-again').classList.add('d-none')
-    intervalChange = setInterval(change, 100)
-    document.querySelectorAll('.a .btn').forEach((item) => {
-      item.removeAttribute('disabled')
-      item.classList.contains('active') && item.classList.remove('active')
-      document.querySelector('.notification').innerHTML = ''
-    })
+// Play again
+document.querySelector('.play-again').addEventListener('click', (event) => {
+  interval = setInterval(change, 100)
+  document.querySelectorAll('.user').forEach((item) => {
+    item.style.pointerEvents = ''
+    item.classList.remove('active')
   })
+  document
+    .querySelector('.notification .alert')
+    .classList.remove('alert-dark', 'alert-warning', 'alert-success')
+  document.querySelector('.notification').classList.add('d-none')
+  document.querySelector('.play-again').classList.add('d-none')
+})
