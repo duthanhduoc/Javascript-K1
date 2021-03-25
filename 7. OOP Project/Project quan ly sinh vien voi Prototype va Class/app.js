dbc34b7bc7b6850d66b343e39effaa251ecbc838
Duoc <duthanhduoc@gmail.com>
2021-03-25 22:26:40 +0700

    update

--- 7. OOP Project/Project quan ly sinh vien voi Prototype va Class/app.js
+++ 7. OOP Project/Project quan ly sinh vien voi Prototype va Class/app.js
@@ -1,129 +1,138 @@
+// Prototype
 // function Student(name, birthday) {
 //   this.name = name
 //   this.birthday = birthday
 // }
+
 // function UI() {}
-// UI.prototype.addStudent = function (student) {
-//   const trNode = document.createElement('tr')
-//   trNode.innerHTML = `
+
+// UI.prototype.add = function (student, order = undefined) {
+//   const tr = document.createElement('tr')
+//   const store = new Store()
+//   const listStudent = store.getList()
+//   tr.innerHTML = `
+//   <td>${order === undefined ? listStudent.length + 1 : order}</td>
 //   <td>${student.name}</td>
 //   <td>${student.birthday}</td>
-//   <td>
-//     <button class="btn btn-danger btn-sm">Xóa</button>
-//   </td>
+//   <td><button class="btn btn-danger">Xóa</button></td>
 //   `
-//   document.querySelector('table tbody').appendChild(trNode)
+//   document.querySelector('table tbody').appendChild(tr)
 // }
-// UI.prototype.clearFields = function () {
-//   document.getElementById('name').value = ''
-//   document.getElementById('birthday').value = ''
+// UI.prototype.render = function () {
+//   const store = new Store()
+//   const listStudent = store.getList()
+//   listStudent.forEach((student, index) => {
+//     this.add(student, index + 1)
+//   })
 // }
-// UI.prototype.removeStudent = function (target) {
-//   const trNode = target.parentElement.parentElement
-//   trNode.remove()
+// UI.prototype.clearAll = function () {
+//   document.querySelector('table tbody').innerHTML = ''
 // }
-// UI.prototype.alert = function (message, className) {
-//   const alertNode = document.createElement('div')
-//   alertNode.classList.add('alert', 'text-center', className)
-//   alertNode.textContent = message
-//   document.querySelector('form').appendChild(alertNode)
+// UI.prototype.alert = function (type, message) {
+//   const alert = document.createElement('div')
+//   alert.className = `alert alert-${type}`
+//   alert.textContent = message
+//   document.querySelector('.notification').appendChild(alert)
 //   setTimeout(() => {
-//     alertNode.remove()
+//     alert.remove()
 //   }, 2000)
 // }
+
+// function Store() {}
+
+// Store.prototype.getList = function () {
+//   return JSON.parse(localStorage.getItem('student')) || []
+// }
+// Store.prototype.add = function (student) {
+//   const listStudent = this.getList()
+//   listStudent.push(student)
+//   localStorage.setItem('student', JSON.stringify(listStudent))
+// }
+// Store.prototype.remove = function (index) {
+//   const listStudent = this.getList()
+//   listStudent.splice(index, 1)
+//   localStorage.setItem('student', JSON.stringify(listStudent))
+// }
+
+// Class
 class Student {
-  constructor(name, birthday, id) {
+  constructor(name, birthday) {
     this.name = name
     this.birthday = birthday
-    this.id = id
   }
 }
+
 class UI {
-  addStudent(student) {
-    const trNode = document.createElement('tr')
-    trNode.innerHTML = `
-      <td>${student.name}</td>
-      <td>${student.birthday}</td>
-      <td>${student.id}</td>
-      <td>
-        <button class="btn btn-danger btn-sm">Xóa</button>
-      </td>
-      `
-    document.querySelector('table tbody').appendChild(trNode)
+  static add(student, order = undefined) {
+    const tr = document.createElement('tr')
+    const listStudent = Store.getList()
+    tr.innerHTML = `
+    <td>${order === undefined ? listStudent.length + 1 : order}</td>
+    <td>${student.name}</td>
+    <td>${student.birthday}</td>
+    <td><button class="btn btn-danger">Xóa</button></td>
+    `
+    document.querySelector('table tbody').appendChild(tr)
   }
-  clearFields() {
-    document.getElementById('name').value = ''
-    document.getElementById('birthday').value = ''
-    document.getElementById('id').value = ''
+  static render() {
+    const listStudent = Store.getList()
+    listStudent.forEach((student, index) => {
+      this.add(student, index + 1)
+    })
   }
-  removeStudent(target) {
-    const trNode = target.parentElement.parentElement
-    trNode.remove()
+  static clearAll() {
+    document.querySelector('table tbody').innerHTML = ''
   }
-  alert(message, className) {
-    const alertNode = document.createElement('div')
-    alertNode.classList.add('alert', 'text-center', className)
-    alertNode.textContent = message
-    document.querySelector('form').appendChild(alertNode)
+  static alert(type, message) {
+    const alert = document.createElement('div')
+    alert.className = `alert alert-${type}`
+    alert.textContent = message
+    document.querySelector('.notification').appendChild(alert)
     setTimeout(() => {
-      alertNode.remove()
+      alert.remove()
     }, 2000)
   }
 }
 
 class Store {
-  static getStudents() {
-    let students = []
-    if (localStorage.getItem('students') !== null) {
-      students = JSON.parse(localStorage.getItem('students'))
-    }
-    return students
+  static getList() {
+    return JSON.parse(localStorage.getItem('student')) || []
   }
-  static displayStudents() {
-    const ui = new UI()
-    Store.getStudents().forEach((student) => {
-      ui.addStudent(student)
-    })
+  static add(student) {
+    const listStudent = this.getList()
+    listStudent.push(student)
+    localStorage.setItem('student', JSON.stringify(listStudent))
   }
-  static addStudent(student) {
-    const students = Store.getStudents()
-    students.push(student)
-    localStorage.setItem('students', JSON.stringify(students))
-  }
-  static removeStudent(id) {
-    const students = Store.getStudents()
-    const index = students.findIndex((item) => item.id === id)
-    students.splice(index, 1)
-    localStorage.setItem('students', JSON.stringify(students))
+  static remove(index) {
+    const listStudent = this.getList()
+    listStudent.splice(index, 1)
+    localStorage.setItem('student', JSON.stringify(listStudent))
   }
 }
-
-window.addEventListener('DOMContentLoaded', Store.displayStudents)
 
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault()
   const name = document.getElementById('name').value
   const birthday = document.getElementById('birthday').value
-  const id = document.getElementById('id').value
-  const ui = new UI()
-  if (name === '' || birthday === '' || id === '') {
-    ui.alert('Hãy điền đầy đủ các trường', 'alert-warning')
-  } else {
-    const student = new Student(name, birthday, id)
-    ui.addStudent(student)
-    Store.addStudent(student)
-    ui.clearFields()
-    ui.alert('Thêm thành công', 'alert-success')
-  }
+  const student = new Student(name, birthday)
+  UI.add(student)
+  Store.add(student)
+  UI.alert('success', `Bạn vừa thêm thành công ${name}`)
 })
 
-document.querySelector('table').addEventListener('click', (event) => {
+UI.render()
+
+document.querySelector('table tbody').addEventListener('click', (event) => {
   if (event.target.classList.contains('btn-danger')) {
-    const ui = new UI()
-    ui.removeStudent(event.target)
-    Store.removeStudent(
-      event.target.parentElement.previousElementSibling.textContent
-    )
-    ui.alert('Xóa thành công', 'alert-success')
+    const tr = event.target.parentElement.parentElement
+    const name = tr.children[1].textContent
+    const index = Number(tr.children[0].textContent) - 1
+    const isCheck = confirm(`Bạn có muốn xóa ${name}?`)
+    if (isCheck) {
+      Store.remove(index)
+      UI.clearAll()
+      UI.render()
+      UI.alert('danger', `Bạn vừa xóa thành công ${name}`)
+    }
   }
 })
